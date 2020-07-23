c7e77ec405bea2d3af28b0a6a579540d33fd8c2b
Lucas Patricio <lucas_patricio@outlook.com.br>
2020-07-23 17:21:01 -0300

    Adicionando programa principal e aplicando algumas correcoes a entidades.

--- ImaginaryStore/Entities/Order.cs
+++ ImaginaryStore/Entities/Order.cs
@@ -1,6 +1,8 @@
 ﻿using ImaginaryStore.Entities.Enums;
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
 
 namespace ImaginaryStore.Entities
 {
@@ -10,25 +12,54 @@ namespace ImaginaryStore.Entities
         public OrderStatus Status { get; set; }
         public Client Client { get; set; }
         public List<OrderItem> Items { get; set; } = new List<OrderItem>();
-
-        public Order(DateTime moment, OrderStatus status, Client client) {
-            Moment = moment;
+      
+        public Order()
+        {
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
             Status = status;
             Client = client;
+            Moment = moment;
         }
 
-        public void AddItem(OrderItem item) {
+        public void AddItem(OrderItem item)
+        {
             Items.Add(item);
         }
-        public void RemoveItem(OrderItem item) {
+        public void RemoveItem(OrderItem item)
+        {
             Items.Remove(item);
         }
-        public double Total() {
+        public double Total()
+        {
             double sum = 0;
-            foreach (OrderItem item in Items) {
-                sum += item.Price;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
             }
             return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder();
+            text.Append("Order moment: ");
+            text.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            text.AppendLine("Order status: " + Status);
+            text.Append("Client: ");
+            text.AppendLine(Client.Name + " (" + Client.BirthDate.ToString("dd/MM/yyyy") + ") - " + Client.Email);
+            text.AppendLine("\nOrder items: ");
+
+            foreach (OrderItem item in Items) {
+                text.Append(item.Product.Name + ", ");
+                text.Append("$" + item.Price.ToString("F2", CultureInfo.InvariantCulture) + ", ");
+                text.Append("Quantity: " + item.Quantity + ", ");
+                text.AppendLine("Subtotal: $" + item.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            text.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return text.ToString();
         }
     }
 }
--- ImaginaryStore/Program.cs
+++ ImaginaryStore/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ImaginaryStore.Entities;
+using ImaginaryStore.Entities.Enums;
+using System;
+using System.Globalization;
 
 namespace ImaginaryStore
 {
@@ -6,7 +9,45 @@ namespace ImaginaryStore
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+
+            Console.WriteLine("\nEnter order data: ");
+            Console.Write("Status: ");
+            OrderStatus os = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order order = new Order(DateTime.Now, os, client);
+
+            Console.Write("How many items to this order? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine("Enter #{0} item data:", i);
+                Console.Write("Product name: ");
+                string prodName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double prodPrice = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int prodQuant = Convert.ToInt32(Console.ReadLine());
+
+                Product prod = new Product(prodName, prodPrice);
+                OrderItem orderItem = new OrderItem(prodQuant, prodPrice, prod);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine("\nORDER SUMARY:");
+            Console.WriteLine(order);
+
         }
     }
 }
